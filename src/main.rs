ab0071f64e6fc20b035d399e0a6b285a49cb429c
dat-thanh-nguyen-gfg <dat.nguyenthanh@global-fashion-group.com>
2026-03-01 10:24:15 +0700

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -5,9 +5,15 @@ use std::env;
 
 #[allow(dead_code)]
 fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
-    // If encoded_value starts with a digit, it's a number
-    if encoded_value.chars().next().unwrap().is_ascii_digit() {
-        // Example: "5:hello" -> "hello"
+    // If encoded_value starts with 'i', it's an integer: i<number>e
+    if encoded_value.starts_with('i') {
+        let end_index = encoded_value.find('e').unwrap();
+        let number_string = &encoded_value[1..end_index];
+        let number = number_string.parse::<i64>().unwrap();
+        serde_json::Value::Number(serde_json::Number::from(number))
+    }
+    // If encoded_value starts with a digit, it's a string: <length>:<data>
+    else if encoded_value.chars().next().unwrap().is_ascii_digit() {
         let colon_index = encoded_value.find(':').unwrap();
         let number_string = &encoded_value[..colon_index];
         let number = number_string.parse::<usize>().unwrap();
