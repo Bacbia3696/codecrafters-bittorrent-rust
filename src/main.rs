5d652a3b2de64122fca3993e18593e13dc019a81
dat-thanh-nguyen-gfg <dat.nguyenthanh@global-fashion-group.com>
2026-03-01 10:38:09 +0700

    decode list

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,47 @@
 use std::env;
 
-// Available if you need it!
-// use serde_bencode
-
 #[allow(dead_code)]
-fn decode_bencoded_value(encoded_value: &str) -> serde_json::Value {
+fn decode_bencoded_value(encoded_value: &str) -> (serde_json::Value, usize) {
     // If encoded_value starts with 'i', it's an integer: i<number>e
     if encoded_value.starts_with('i') {
         let end_index = encoded_value.find('e').unwrap();
         let number_string = &encoded_value[1..end_index];
         let number = number_string.parse::<i64>().unwrap();
-        serde_json::Value::Number(serde_json::Number::from(number))
+        (
+            serde_json::Value::Number(serde_json::Number::from(number)),
+            end_index + 1,
+        )
     }
     // If encoded_value starts with a digit, it's a string: <length>:<data>
     else if encoded_value.chars().next().unwrap().is_ascii_digit() {
         let colon_index = encoded_value.find(':').unwrap();
-        let number_string = &encoded_value[..colon_index];
-        let number = number_string.parse::<usize>().unwrap();
-        let string = &encoded_value[colon_index + 1..colon_index + 1 + number];
-        serde_json::Value::String(string.to_string())
+        let length_string = &encoded_value[..colon_index];
+        let length = length_string.parse::<usize>().unwrap();
+        let string = &encoded_value[colon_index + 1..colon_index + 1 + length];
+        (
+            serde_json::Value::String(string.to_string()),
+            colon_index + 1 + length,
+        )
+    }
+    // If encoded_value starts with 'l', it's a list: l<elements>e
+    else if encoded_value.starts_with('l') {
+        let mut elements = Vec::new();
+        let mut pos = 1; // Skip the 'l'
+
+        while pos < encoded_value.len() && encoded_value.as_bytes()[pos] != b'e' {
+            let (element, consumed) = decode_bencoded_value(&encoded_value[pos..]);
+            elements.push(element);
+            pos += consumed;
+        }
+
+        (serde_json::Value::Array(elements), pos + 1) // +1 to skip the 'e'
     } else {
         panic!("Unhandled encoded value: {}", encoded_value)
     }
+}
+
+fn decode_bencoded_value_simple(encoded_value: &str) -> serde_json::Value {
+    decode_bencoded_value(encoded_value).0
 }
 
 // Usage: your_program.sh decode "<encoded_value>"
@@ -33,10 +53,9 @@ fn main() {
         // You can use print statements as follows for debugging, they'll be visible when running tests.
         eprintln!("Logs from your program will appear here!");
 
-        // TODO: Uncomment the code below to pass the first stage
         let encoded_value = &args[2];
-        let decoded_value = decode_bencoded_value(encoded_value);
-        println!("{}", decoded_value.to_string());
+        let decoded_value = decode_bencoded_value_simple(encoded_value);
+        println!("{}", decoded_value);
     } else {
         println!("unknown command: {}", args[1])
     }
